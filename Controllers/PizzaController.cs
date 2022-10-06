f7cd903af2c750a27dcd32184519dc20f207c9f4
agostinopique <agostino.pique@gmail.com>
2022-10-06 15:39:58 +0200

    Aggiunta modifica degli elementi;

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -7,19 +7,6 @@ namespace la_mia_pizzeria_static.Controllers
 {
     public class PizzaController : Controller
     {
-        
-        //public List<Pizza> GeneratePizzas()
-        //{
-        //    List<Pizza> pizzas = new List<Pizza> {
-        //                new Pizza("Margherita", "Pizza con pomodoro, mozzarella e basilico", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Pizza_Margherita_stu_spivack.jpg/1280px-Pizza_Margherita_stu_spivack.jpg", "5"),
-        //                new Pizza("Marinara", "Pizza con pomodoro, mozzarella e basilico", "https://www.buttalapasta.it/wp-content/uploads/2021/03/pizza-marinara.jpg","4"),
-        //                new Pizza("Napoli", "Pizza napoletana", "https://wips.plug.it/cips/buonissimo.org/cms/2012/07/pizza-napoli-5.jpg", "6,50"),
-        //                new Pizza("Wurstel", "pizza con pomodoro mozzarella e wurtel", "https://www.lospicchiodaglio.it/img/ricette/pizzawurstel.jpg", "6"),
-        //                new Pizza("Salamino piccante",  "Pizza con pomodoro, mozzarella e salamino piccante", "https://api.molinocasillo.com/wp-content/uploads/2020/07/pizza.jpg", "6"),
-        //                new Pizza("Maialona", "Pizza margherita con wurstel, salsiccia, salamino picccante", "https://media-cdn.tripadvisor.com/media/photo-s/13/a4/63/f8/pizza-maialona.jpg", "7")
-        //            };
-        //    return pizzas;
-        //}
 
         public IActionResult Index()
         {
@@ -55,7 +42,7 @@ namespace la_mia_pizzeria_static.Controllers
             Pizza pizza;
             using (PizzeriaContext db = new PizzeriaContext())
             {
-                pizza = db.Pizze.Where(pizze => pizze.Id == id).First();
+                pizza = db.Pizze.Where(pizze => pizze.Id == id).FirstOrDefault();
             }
                 
 
@@ -86,5 +73,39 @@ namespace la_mia_pizzeria_static.Controllers
             
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            PizzeriaContext db = new PizzeriaContext();
+
+            Pizza pizzaToUpdate = db.Pizze.Where(pizze => pizze.Id == id).FirstOrDefault();
+            return View("EditPizza", pizzaToUpdate);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza pizzaData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EditPizza", pizzaData);
+            }
+
+            PizzeriaContext db = new PizzeriaContext();
+
+            Pizza pizzaDaModificare = db.Pizze.Where(pizze => pizze.Id == id).FirstOrDefault();
+
+            pizzaDaModificare.Name = pizzaData.Name;
+            pizzaDaModificare.Description = pizzaData.Description;
+            pizzaDaModificare.Price = pizzaData.Price;
+            pizzaDaModificare.Picture = pizzaData.Picture;
+            db.SaveChanges();
+
+            return RedirectToAction("Details", pizzaDaModificare);
+        }
+
+        
     }
 }
