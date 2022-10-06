c5e70c4ec8796d9c01bacc33227a0e5fa4fa68d8
agostinopique <agostino.pique@gmail.com>
2022-10-06 14:39:23 +0200

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+
+    [Table("Pizze")]
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string Price { get; set; }
+
+
+        public Pizza()
+        {
+
+        }
+       public Pizza(string name, string description, string picture, string price)
+        {
+            int id = 0;
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Picture = picture;
+            this.Price = price;
+            id++;
+        }
+    }
+}
---
+++ Models/PizzeriaContext.cs
@@ -0,0 +1,15 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzeriaContext : DbContext
+    {
+        public DbSet<Pizza> Pizze { get; set; } 
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=la-mia-pizzeria-db;Integrated Security=True");
+        }
+    }
+}
